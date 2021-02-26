ade81a06a8d8c739cc3f972fb039246f75c2126b
alex-arakelyan <alex.arakelyan@epicgames.com>
2021-02-26 16:43:29 +0400

    adding response if success

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 def integration_test():
     expected_response = "Hello World!"
     r = requests.get('http://localhost:8088/')
+    print("Response OK")
     assert r.text == expected_response, "Wrong Response"
 
 if __name__ == '__main__':
