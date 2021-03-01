0cb64f0324f793b05fa35a0ad9f14972f1eec6b6
alexarakelyan <alexarakelyan94@gmail.com>
2021-03-01 14:21:22 +0400

    I know I've already sent you the repo but I just noticed this minor typo :)

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import requests
 def integration_test():
     expected_response = "Hello World!"
     r = requests.get('http://localhost:8088/')
-    print("Response OK")
     assert r.text == expected_response, "Wrong Response"
+    print("Response OK")
 
 if __name__ == '__main__':
     integration_test()
